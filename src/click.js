07a09cf8864a910c4bf349f3bdd00235bf917416
ZFous <remerson.tomaz@academico.ifpb.edu.br>
2025-12-05 21:52:09 -0300

    Update click.js

--- src/click.js
+++ src/click.js
@@ -1,43 +1,41 @@
 let incrementarButton = document.getElementById('incrementarButton');
 
-let incremento =
-  localStorage.getItem('incremento') == null
-    ? { valor: 1 }
-    : JSON.parse(localStorage.getItem('incremento'));
-//  condicao ? verdadeira : falsa
-// if (localStorage.getItem('incremento') == null) {
-// }
-let j = 1;
-while (j < incremento.valor) {
-  // Recuperar o elemento da lista desordenada
-  let itensUl = document.getElementById('itensUl');
-  // Adicionar o item com o valor do incremento da lista desordenada.
-  itensUl.insertAdjacentHTML('beforeend', `<li>${j}</li>`);
-  j++;
-}
+let incremento; 
 
-// Evento de click do botão incrementar.
-incrementarButton.onclick = (event) => {
-  // Recuperar o elemento da lista desordenada
+window.onload = function () {
+
+  incremento =
+    localStorage.getItem('incremento') == null
+      ? { valor: 1 }
+      : JSON.parse(localStorage.getItem('incremento'));
+
+
+  let j = 1;
+  while (j < incremento.valor) {
+    let itensUl = document.getElementById('itensUl');
+    itensUl.insertAdjacentHTML('beforeend', `<li>${j}</li>`);
+    j++;
+  }
+};
+
+
+incrementarButton.onclick = () => {
   let itensUl = document.getElementById('itensUl');
-  // Adicionar o item com o valor do incremento da lista desordenada.
   itensUl.insertAdjacentHTML('beforeend', `<li>${incremento.valor}</li>`);
 
-  // Guardar o valor do incremento numa estrutura de armazenamento: local storage.
-  // O valor deverá ser persistido em formato json através de uma string.
   incremento.valor++;
   localStorage.setItem('incremento', JSON.stringify(incremento));
 };
 
-incrementarButton.onmouseover = (event) => {
+
+incrementarButton.onmouseover = () => {
   incrementarButton.style.backgroundColor = 'blue';
 };
 
-incrementarButton.onmouseout = (event) => {
+incrementarButton.onmouseout = () => {
   incrementarButton.style.backgroundColor = 'gray';
 };
 
-incrementarButton.addEventListener('dblclick', (event) => {
+incrementarButton.addEventListener('dblclick', () => {
   console.log('Clicou no dblclick');
 });
-// callback
